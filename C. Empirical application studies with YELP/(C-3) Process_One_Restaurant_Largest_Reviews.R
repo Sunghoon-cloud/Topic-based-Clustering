dc3aeee4b80e40d948038a76636d84885c291feb
Sunghoon Kim <75557269+Sunghoon-cloud@users.noreply.github.com>
2024-01-25 17:44:52 -0500

    Delete C. Empirical application studies with YELP directory

--- C. Empirical application studies with YELP/(C-3) Process_One_Restaurant_Largest_Reviews.R
+++
@@ -1,2 +0,0 @@
-
-This code extract the dataset (e.g., An Italian restaurant with the largest number of reviews) from the original Yelp data for customer-level segmentation study in the second empirical application study. If model user wants to see the results relatively quickly, the dataset is recommended given the size is smaller and running time is faster. 
